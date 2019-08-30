d257c4cd4531b5110ca5a455a34d72a35c24cb70
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2019-08-31 01:48:42 +0200

    Create stop-forum-spam-checker.meta.js

---
+++ src/stop-forum-spam-checker.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         WordPress.org Stop Foum Spam Checker
+// @namespace    https://wordpress.org/support
+// @version      1.0.0
+// @description  Check post author IPs against the Stop Forum Spam API to determine their credibility.
+// @author       Clorith
+// @match        https://wordpress.org/support/topic/*
+// @match        https://wordpress.org/support/view/pending/
+// @match        https://wordpress.org/support/view/spam/
+// @match        https://wordpress.org/support/?post_type=topic&*
+// @match        https://*.wordpress.org/support/topic/*
+// @match        https://*.wordpress.org/support/view/pending/
+// @match        https://*.wordpress.org/support/view/spam/
+// @match        https://*.wordpress.org/support/?post_type=topic&*
+// @require      https://code.jquery.com/jquery-1.11.0.min.js
+// @updateURL    https://github.com/wporg-support/stop-forum-spam-checker/raw/master/src/stop-forum-spam-checker.user.js
+// @downloadURL  https://github.com/wporg-support/stop-forum-spam-checker/raw/master/src/stop-forum-spam-checker.user.js
+// @grant        none
+// ==/UserScript==
